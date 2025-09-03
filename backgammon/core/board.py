d81a35e790a490064954e4e60699d12581862ee6
Jeremías Villach <jerevillach@gmail.com>
2025-09-03 01:16:44 +0000

    Agregar método reset_board() en Board

--- backgammon/core/board.py
+++ backgammon/core/board.py
@@ -1,4 +1,12 @@
 class Board:
     """Representa el tablero de Backgammon con 24 puntos."""
+
     def __init__(self):
         self.__puntos__ = [0] * 24
+
+    def reset_board(self):
+        """
+        Inicializa el tablero en estado vacío (todas las posiciones con 0).
+        """
+        self.__puntos__ = [0] * 24
+        return self.__puntos__
