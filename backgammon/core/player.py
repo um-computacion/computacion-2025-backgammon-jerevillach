7bee68da827baabfa0d4eb12bb5b9b4576099a47
Jeremías Villach <jerevillach@gmail.com>
2025-09-09 22:09:22 +0000

    Agregar método add_checker() en Player

--- backgammon/core/player.py
+++ backgammon/core/player.py
@@ -3,4 +3,12 @@ class Player:
     def __init__(self, nombre: str):
         self.__nombre__ = nombre
         self.__fichas__ = 15
+    def add_checker(self):
+        """
+        Agrega una ficha al jugador.
+        """
+        self.__fichas__ += 1
+        return self.__fichas__
 
+ 
+   
