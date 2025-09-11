a3082dd4e598aa74cb804db50918370d34ff0392
Jeremías Villach <jerevillach@gmail.com>
2025-09-11 22:51:02 +0000

    Agregar método tirar_dados() en BackgammonGame

--- backgammon/core/game.py
+++ backgammon/core/game.py
@@ -4,8 +4,15 @@ from .dice import Dice
 
 class BackgammonGame:
     """Coordina el flujo general del juego."""
+
     def __init__(self, jugador1: str, jugador2: str):
         self.__board__ = Board()
         self.__player1__ = Player(jugador1)
         self.__player2__ = Player(jugador2)
         self.__dice__ = Dice()
+
+    def tirar_dados(self):
+        """
+        Llama a los dados y devuelve el resultado de la tirada.
+        """
+        return self.__dice__.roll()
