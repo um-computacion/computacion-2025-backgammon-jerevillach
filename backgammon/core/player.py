50f3619337cc2a06e45f553efae6abe2e637bf6b
Jeremías Villach <jerevillach@gmail.com>
2025-09-15 18:03:44 +0000

    Agregar método remove_checker() en Player

--- backgammon/core/player.py
+++ backgammon/core/player.py
@@ -1,14 +1,22 @@
 class Player:
     """Representa a un jugador con sus fichas."""
+
     def __init__(self, nombre: str):
         self.__nombre__ = nombre
         self.__fichas__ = 15
+
     def add_checker(self):
-        """
-        Agrega una ficha al jugador.
-        """
         self.__fichas__ += 1
         return self.__fichas__
+
+    def remove_checker(self):
+        """
+        Resta una ficha al jugador (si tiene fichas disponibles).
+        """
+        if self.__fichas__ > 0:
+            self.__fichas__ -= 1
+        return self.__fichas__
+
 
  
    
